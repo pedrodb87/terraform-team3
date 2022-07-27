54cc37400643d38b71d5b87c0cf5fada3fcbfe64
root <root@terraform-team3.us-west4-b.c.plucky-tract-350819.internal>
2022-07-27 02:47:49 +0000

    adding terraform and providers file

---
+++ terra-installation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+git clone --depth=1 https://github.com/tfutils/tfenv.git ~/.tfenv
+echo 'export PATH="$HOME/.tfenv/bin:$PATH"' >> ~/.bash_profile
+sudo ln -s ~/.tfenv/bin/* /usr/local/bin
+source ~/.bash_profile
