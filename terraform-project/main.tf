8d84015c396aaff248ab7465c3f721db78db8dd6
root <root@terraform-team3.us-west4-b.c.plucky-tract-350819.internal>
2022-07-28 20:49:13 +0000

    updating code

--- terraform-project/main.tf
+++ terraform-project/main.tf
@@ -5,8 +5,8 @@ resource "google_compute_network" "vpc-network-team3" {
 
 resource "google_compute_autoscaler" "team3" {
     name   = "my-autoscaler-team3"
-  zone   = "us-east1-b"
-  target = google_compute_instance.pedrito-test.id
+  zone   = var.zone
+  target = google_compute_instance_group_manager.my-igm.self_link
   
 
   autoscaling_policy {
@@ -18,55 +18,70 @@ resource "google_compute_autoscaler" "team3" {
 }
 
 
-resource "google_compute_instance_template" "template" {
+resource "google_compute_instance_template" "compute-engine" {
 name = "my-instance-template"
-machine_type = "var.machine_type"
+machine_type = var.machine_type
 can_ip_forward = false
-project = "<PROJECT_NAME>"
-tags = ["foo", "bar", "allow-lb-service"]
+project = var.project_name
+
 
 disk {
 source_image = data.google_compute_image.centos_7.self_link
 }
 
 network_interface {
-network = google_compute_network.vpc_network.name
+network = google_compute_network.vpc-network-team3.name
 }
 }
 
-
-
-
-
-
-
-
-
-
-
-resource "google_compute_instance" "pedrito-test" {
-  name         = "pedrito"
-  machine_type = "e2-small"
-  zone         = "us-east1-b"
-
-  tags = ["team", "three"]
-
-  boot_disk {
-    initialize_params {
-      image = "centos-7-v20220719"
-    }
-  }
-
-  // Local SSD disk
-  # scratch_disk {
-  #   interface = "SCSI"
-  # }
-
-  network_interface {
-    network = google_compute_network.vpc-network-team3.name
-
-    access_config {
-      // Ephemeral public IP
-    }
-  }
+resource "google_compute_instance_group_manager" "my-igm" {
+name = "my-igm"
+zone = var.zone
+project = var.project_name
+version {
+instance_template = google_compute_instance_template.compute-engine.self_link
+name = "primary"
 }
+base_instance_name = "team3"
+}
+
+data "google_compute_image" "centos_7" {
+family = "centos-7"
+project = "centos-cloud"
+}
+
+
+
+
+
+
+
+
+
+
+# resource "google_compute_instance" "pedrito-test" {
+#   name         = "pedrito"
+#   machine_type = "e2-small"
+#   zone         = "us-east1-b"
+
+#   tags = ["team", "three"]
+
+#   boot_disk {
+#     initialize_params {
+#       image = "centos-7-v20220719"
+#     }
+#   }
+
+#   // Local SSD disk
+#   # scratch_disk {
+#   #   interface = "SCSI"
+#   # }
+
+#   network_interface {
+#     network = google_compute_network.vpc-network-team3.name
+
+#     access_config {
+#       // Ephemeral public IP
+#     }
+#   }
+# }
--- terraform-project/variables.tf
+++ terraform-project/variables.tf
@@ -1,5 +1,18 @@
 variable "machine_type" {
-  type        = "e2-small"
-  default     = ""
+  type        = string
+  default     = "e2-small"
   description = "add your machine type"
 }
+
+variable "project_name" {
+  type        = string
+  default     = "plucky-tract-350819"
+  description = "enter your project name"
+}
+
+variable "zone" {
+  type        = string
+  default     = "us-east1-b"
+  description = "description"
+}
+
