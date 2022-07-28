854eea80f447876379c565bbcba74fb7ad5c09cb
root <root@terraform-team3.us-west4-b.c.plucky-tract-350819.internal>
2022-07-28 17:26:36 +0000

    adding the code for VPC

--- terraform-project/main.tf
+++ terraform-project/main.tf
@@ -1,26 +1,36 @@
-resource "google_compute_instance" "pedrito-test" {
-  name         = "pedrito"
-  machine_type = "e2-small"
-  zone         = "us-east1-b"
-
-  tags = ["foo", "bar"]
-
-  boot_disk {
-    initialize_params {
-      image = "centos-7-v20220719"
-    }
-  }
-
-  // Local SSD disk
-  # scratch_disk {
-  #   interface = "SCSI"
-  # }
-
-  network_interface {
-    network = "default"
-
-    access_config {
-      // Ephemeral public IP
-    }
-  }
+resource "google_compute_network" "vpc_network-team3" {
+  name                    = "vpc-network-team"
+  auto_create_subnetworks = "true"
 }
+
+
+
+
+
+
+# resource "google_compute_instance" "pedrito-test" {
+#   name         = "pedrito"
+#   machine_type = "e2-small"
+#   zone         = "us-east1-b"
+
+#   tags = ["foo", "bar"]
+
+#   boot_disk {
+#     initialize_params {
+#       image = "centos-7-v20220719"
+#     }
+#   }
+
+#   // Local SSD disk
+#   # scratch_disk {
+#   #   interface = "SCSI"
+#   # }
+
+#   network_interface {
+#     network = "google_compute_network.vpc_network-team3.id"
+
+#     access_config {
+#       // Ephemeral public IP
+#     }
+#   }
+# }
